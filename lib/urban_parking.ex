56be9f4d98414963ec61d656b6e8f673ec8d1456
Juan Carlos Pazos Mandiá <jucapaman@gmail.com>
2019-01-12 17:45:59 +0100

    Init elixir project

---
+++ lib/urban_parking.ex
@@ -0,0 +1,18 @@
+defmodule UrbanParking do
+  @moduledoc """
+  Documentation for UrbanParking.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UrbanParking.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/urban_parking_test.exs
@@ -0,0 +1,8 @@
+defmodule UrbanParkingTest do
+  use ExUnit.Case
+  doctest UrbanParking
+
+  test "greets the world" do
+    assert UrbanParking.hello() == :world
+  end
+end
